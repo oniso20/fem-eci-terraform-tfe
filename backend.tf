7177583b6ad2f37b85acd850fa3d4e543e8a092e
onis20 <emem.c.onis@gmail.com>
2025-02-09 16:02:45 +0200

    Made some updates to the Terraform project

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
     organization = "Onis_Personal"
 
     workspaces {
-      name = "fem-eci-workspace"
+      name = "fem-eci-tfe"
     }
   }
 }
--- main.tf
+++ main.tf
@@ -1,19 +1,23 @@
 module "project" {
+  for_each = local.project
+
   source  = "ALT-F4-LLC/project/tfe"
   version = "0.4.0"
 
-  description       = "Example description of project"
-  name              = "fem-eci-project"
+  description       = each.value.description
+  name              = each.key
   organization_name = var.organization_name
 }
 
 module "workspace" {
+  for_each = local.workspace
+
   source  = "ALT-F4-LLC/workspace/tfe"
   version = "0.6.0"
 
-  description       = "Example description of workspace"
+  description       = each.value.description
   execution_mode    = "local"
-  name              = "fem-eci-workspace"
+  name              = each.key
   organization_name = var.organization_name
-  project_id        = module.project.id
+  project_id        = each.value.project_id
 }
